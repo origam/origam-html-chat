8e118aefcc00acc689eae3e5ce85bb0521e631b6
Pavel Tomasko <ptomask@gmail.com>
2020-08-28 11:39:17 +0200

    INTERNAL: Observer batching.

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,3 +1,4 @@
+import "mobx-react-lite/batchingForReactDom";
 import React from "react";
 import ReactDOM from "react-dom";
 import {
@@ -43,4 +44,3 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-
